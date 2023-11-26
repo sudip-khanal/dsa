a198fd040fe22145be56b4c2ec8e2aa9f5f3a3f3
sudip-khanal <101724348+sudip-khanal@users.noreply.github.com>
2023-11-26 12:46:39 +0545

    strictly sorted

---
+++ Strictly_sorted.java
@@ -0,0 +1,20 @@
+//Example of strickly sorted arr using recursion
+public class Strictly_sorted {
+    public static boolean isSorted(int arr[], int idx) {
+        if (idx == arr.length - 1) {
+            return true;
+        }
+        if (arr[idx] >= arr[idx + 1]) {
+            return false;
+
+        } else {
+            return isSorted(arr, idx + 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, };
+        System.out.println(isSorted(arr, 0));
+    }
+}
